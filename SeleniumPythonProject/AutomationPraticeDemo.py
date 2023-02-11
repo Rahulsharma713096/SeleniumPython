ffbe94b4e9d3c8ae72bf3b0a9203686132e41feb
RAHUL SHARMA <69028299+Rahulsharma713096@users.noreply.github.com>
2023-02-11 08:10:00 +0530

    created new repositry
    na

--- SeleniumPythonProject/AutomationPraticeDemo.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.delete_all_cookies()
-driver.get("https://www.rahulshettyacademy.com/AutomationPractice/")
-
-
-
